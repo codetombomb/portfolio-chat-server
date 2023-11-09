a45a90f5799455981d325f160c55c455bfb441b9
Tom Tobar <codetombomb@gmail.com>
2023-11-09 17:15:18 -0600

    add cors

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const http = require("http");
 const Server = require("socket.io").Server;
-const app = express();
+const cors = require('cors');
+const app = express(cors());
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
