1b0a1ecd733a0291ad2d53f5a29667368128cd52
Tomas Tobar <codetombomb@gmail.com>
2023-11-26 23:51:53 -0600

    add console.log to monitor logs

--- index.js
+++ index.js
@@ -19,6 +19,7 @@ const URL_BASE = "https://portfolio-api-ws.onrender.com"
 // const URL_BASE = "http://127.0.0.1:5000"
 
 serverIO.on("connection", (socket) => {
+  console.log("Connected!", Date.now())
   socket.on("initChat", () => {
     const newRoom = {
       visitor_id: "",
